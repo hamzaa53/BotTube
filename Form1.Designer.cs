054f7b7b83541ccfd4a939e4c97fd93f606872ad
Hamza <Hamza@HAMZAPC>
2022-06-12 19:37:56 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace BotTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dosyaSec = new System.Windows.Forms.OpenFileDialog();
+            this.yukle = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.yuzde = new System.Windows.Forms.Label();
+            this.sistem = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoSec
+            // 
+            this.videoSec.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.videoSec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.videoSec.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.videoSec.Location = new System.Drawing.Point(12, 12);
+            this.videoSec.Name = "videoSec";
+            this.videoSec.Size = new System.Drawing.Size(226, 60);
+            this.videoSec.TabIndex = 0;
+            this.videoSec.Text = "Video Seç";
+            this.videoSec.UseVisualStyleBackColor = true;
+            this.videoSec.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(8, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Video";
+            this.label1.Visible = false;
+            // 
+            // dosyaSec
+            // 
+            this.dosyaSec.Filter = "Video Dosyası (*.mp4)|*.mp4";
+            this.dosyaSec.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // yukle
+            // 
+            this.yukle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.yukle.Enabled = false;
+            this.yukle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yukle.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yukle.Location = new System.Drawing.Point(12, 172);
+            this.yukle.Name = "yukle";
+            this.yukle.Size = new System.Drawing.Size(226, 60);
+            this.yukle.TabIndex = 2;
+            this.yukle.Text = "Yükle";
+            this.yukle.UseVisualStyleBackColor = true;
+            this.yukle.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown1.Location = new System.Drawing.Point(77, 124);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(161, 30);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tekrar: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(374, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 60);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Video Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "https://www.youtube.com/upload";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(335, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 260);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(201, 23);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 19);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "1. Video Yükleniyor...";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(8, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 19);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "1. Video Yüklendi ✅";
+            this.label4.Visible = false;
+            // 
+            // yuzde
+            // 
+            this.yuzde.AutoSize = true;
+            this.yuzde.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F);
+            this.yuzde.Location = new System.Drawing.Point(214, 263);
+            this.yuzde.Name = "yuzde";
+            this.yuzde.Size = new System.Drawing.Size(25, 18);
+            this.yuzde.TabIndex = 13;
+            this.yuzde.Text = "%0";
+            this.yuzde.Visible = false;
+            // 
+            // sistem
+            // 
+            this.sistem.Interval = 500;
+            this.sistem.Tick += new System.EventHandler(this.sistem_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 343);
+            this.Controls.Add(this.yuzde);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.yukle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.videoSec);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BotTube";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button videoSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog dosyaSec;
+        private System.Windows.Forms.Button yukle;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label yuzde;
+        private System.Windows.Forms.Timer sistem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Diagnostics;
+using System.IO;
+
+namespace BotTube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChromeDriverService servis = ChromeDriverService.CreateDefaultService();
+        ChromeDriver tarayici; ChromeOptions options;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string kullaniciAdi = SystemInformation.UserName;
+            options = new ChromeOptions(); servis.HideCommandPromptWindow = true;
+            options.AddArgument(@"user-data-dir=C:/Users/" + kullaniciAdi + "/AppData/Local/Google/Chrome/User Data");
+            //tarayici = new ChromeDriver(servis, options);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dosyaSec.ShowDialog();
+        }
+
+        bool tarayiciAcik;
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (tarayiciAcik) tarayici.Quit();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DosyaBilgileri(dosyaSec.FileName);
+        }
+
+        bool buton = true;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (buton)
+            {
+                DialogResult cevap = MessageBox.Show($"Başlamadan önce tüm Chrome pencereleri kapatılacak, yaptığınız değişiklikler kaydedilmemiş olabilir yine de devam etmek istiyor musunuz?", "Başlat", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (cevap == DialogResult.Yes)
+                {
+                    buton = false; yukle.Text = "Durdur";
+                    if (!tarayiciAcik) { ChromeKapat(); ChromeAc(); }
+                    //VideoyuPaylas();
+                    sistem.Start();
+                }
+            }
+            else
+            {
+                buton = true; yukle.Text = "Yeniden Başlat";
+                tarayici.Close(); tarayiciAcik = false;
+            }
+        }
+
+        void DosyaBilgileri(string yol)
+        {
+            FileInfo dosya = new FileInfo(yol);
+            float dosyaBoyutu = (float)dosya.Length / (1024 * 1024);
+            label1.Text = $"Video: {dosyaSec.SafeFileName}\nBoyut: {Math.Round(dosyaBoyutu, 2)} MB";
+            label1.Visible = true;
+            yukle.Enabled = true;
+        }
+
+        string Sec(string komut)
+        {
+            switch (komut)
+            {
+                case "Hayır":
+                    return "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[5]/ytkc-made-for-kids-select/div[4]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[2]/div[2]/ytcp-ve";
+                case "İleri":
+                    return "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[2]/ytcp-button[2]";
+                case "Herkes":
+                    return "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-uploads-review/div[2]/div[1]/ytcp-video-visibility-select/div[1]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[3]";
+                case "Kaydet":
+                    return "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[2]/ytcp-button[3]";
+                case "Kalan":
+                    return "/html/body/ytcp-uploads-still-processing-dialog/ytcp-dialog/tp-yt-paper-dialog/div[2]/div/ytcp-video-upload-progress/span";
+                default: return "";
+            }
+        }
+
+        void Bekle(int milisaniye) => System.Threading.Thread.Sleep(milisaniye);
+
+        void ButonaTikla(string yol) 
+        {
+            try { tarayici.FindElement(By.XPath(yol)).Click(); } catch { }
+        }
+
+        void DosyaYukle()
+        {
+            try { tarayici.FindElement(By.XPath("//input[@type='file']")).SendKeys(dosyaSec.FileName); } catch { }
+        }
+
+        int YuklemeYuzdesi()
+        {
+            string metin = tarayici.FindElement(By.XPath(Sec("Kalan"))).GetAttribute("innerText");
+            if (metin.Split(' ')[0] != "İşlenme")
+            {
+                string yuzde = metin.Split(' ')[2];
+                int deger = Convert.ToInt32(yuzde.Remove(0, 1));
+                return deger;
+            } else return 100;
+        }
+
+        void ChromeKapat()
+        {
+            Process[] uygulamalar = Process.GetProcessesByName("Chrome");
+            foreach (var chrome in uygulamalar) chrome.Kill();
+        }
+
+        void ChromeAc()
+        {
+            tarayici = new ChromeDriver(servis, options); tarayiciAcik = true;
+        }
+
+        void VideoyuPaylas()
+        {
+            tarayici.Navigate().GoToUrl("https://www.youtube.com/upload");
+            try { var uyari = tarayici.SwitchTo().Alert(); uyari.Accept(); } catch { }
+            Bekle(10000);
+            DosyaYukle();
+            Bekle(5000);
+            ButonaTikla(Sec("Hayır"));
+            ButonaTikla(Sec("İleri"));
+            ButonaTikla(Sec("İleri"));
+            ButonaTikla(Sec("İleri"));
+            Bekle(5000);
+            ButonaTikla(Sec("Herkes"));
+            ButonaTikla(Sec("Kaydet"));
+            Bekle(5000);
+            timer1.Start();
+            label3.Text = ++paylasilanVideo + ". Video Yükleniyor"; label3.Visible = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        uint paylasilanVideo = 0;
+        string yuklenenVideo = "";
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = YuklemeYuzdesi();
+            yuzde.Text = "%" + progressBar1.Value.ToString();
+
+            string metin = tarayici.FindElement(By.XPath(Sec("Kalan"))).GetAttribute("innerText");
+            try { label4.Text = $"Tahmini Süre: {metin.Split(' ')[4]} {metin.Split(' ')[5]}\n{yuklenenVideo}"; } catch { label4.Text = yuklenenVideo; }
+
+            if (YuklemeYuzdesi() == 100)
+            {
+                timer1.Stop();
+                paylasilanVideo++;
+                videoSec.Text = paylasilanVideo.ToString();
+                if (paylasilanVideo == numericUpDown1.Value + 1)
+                {
+                    label4.Visible = false;
+                    label3.Text = "Tüm Videolar Yüklendi ✅";
+                }
+                else
+                {
+                    yuklenenVideo = paylasilanVideo + ". Video Yüklendi ✅";
+                    progressBar1.Value = 0;
+                    yuzde.Text = "%0";
+                    sistem.Start(); 
+                }
+            } else {  }
+        }
+
+        uint adim = 0;
+        uint hata = 20;
+        private void sistem_Tick(object sender, EventArgs e)
+        {
+            switch (adim)
+            {
+                case 0:
+                    tarayici.Navigate().GoToUrl("https://www.youtube.com/upload");
+                    try { var uyari = tarayici.SwitchTo().Alert(); uyari.Accept(); } catch { }
+                    adim++;
+                    break;
+                case 1:
+                    try { if (tarayici.FindElement(By.XPath("//input[@type='file']")).Enabled) { DosyaYukle(); adim++; hata = 20; } }
+                    catch
+                    {
+                        hata--; Text = hata.ToString(); if (hata == 0)
+                        { sistem.Stop(); MessageBox.Show("Yüklenecek konum bulunamadı lütfen tekrar yüklemeyi deneyin.", "Konum bulunamadı", MessageBoxButtons.OK, MessageBoxIcon.Error); break; } 
+                    }
+                    break;
+                case 2:
+                    try { if (tarayici.FindElement(By.XPath(Sec("Hayır"))).Enabled) { ButonaTikla(Sec("Hayır")); adim++; hata = 20; } }
+                    catch
+                    {
+                        hata--; Text = hata.ToString(); if (hata == 0)
+                        { sistem.Stop(); MessageBox.Show("Yüklenecek konum bulunamadı lütfen tekrar yüklemeyi deneyin.", "Konum bulunamadı", MessageBoxButtons.OK, MessageBoxIcon.Error); break; } 
+                    }
+                    break;
+                case 3:
+                    ButonaTikla(Sec("İleri"));
+                    ButonaTikla(Sec("İleri"));
+                    ButonaTikla(Sec("İleri"));
+                    adim++;
+                    break;
+                case 4:
+                    try { if (tarayici.FindElement(By.XPath(Sec("Kaydet"))).GetAttribute("aria-disabled").ToString() == "false") { ButonaTikla(Sec("Herkes")); ButonaTikla(Sec("Kaydet")); adim++; hata = 20; } }
+                    catch
+                    {
+                        hata--; Text = hata.ToString(); if (hata == 0)
+                        { sistem.Stop(); MessageBox.Show("İşlem yapılacak yol bulunamadı lütfen tekrar yüklemeyi deneyin.", "Yol bulunamadı", MessageBoxButtons.OK, MessageBoxIcon.Error); break; }
+                    }
+                    break;
+                case 5:
+                    try 
+                    { 
+                        if (tarayici.FindElement(By.XPath(Sec("Kalan"))).Enabled) 
+                        { 
+                            sistem.Stop(); 
+                            timer1.Start(); 
+                            adim = 0; hata = 20; 
+                            label3.Text = 1 + paylasilanVideo + ". Video Yükleniyor"; 
+                            label3.Visible = true; label4.Visible = true;
+                            progressBar1.Visible = true; yuzde.Visible = true;
+                        } 
+                    }
+                    catch
+                    {
+                        hata--; Text = hata.ToString(); if (hata == 0)
+                        { sistem.Stop(); MessageBox.Show("İşlem yapılacak yol bulunamadı lütfen tekrar yüklemeyi deneyin.", "Yol bulunamadı", MessageBoxButtons.OK, MessageBoxIcon.Error); break; }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            yukle.Text = tarayici.FindElement(By.XPath(Sec("Kaydet"))).GetAttribute("aria-disabled").ToString();
+        }
+    }
+}
